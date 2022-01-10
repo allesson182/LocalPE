ff6a4cfa2b8f77c8a073f5dd3ed555810708657e
Victor Guthierry Dias da Silva <victor.guthierry.dias@gmail.com>
2022-01-10 00:04:12 -0300

    Cards de lugar

--- screens/homeScreen.js
+++ screens/homeScreen.js
@@ -1,14 +1,15 @@
 import { NavigationContainer } from '@react-navigation/native';
 import React, {useState} from 'react';
-import { StyleSheet, Text, View, Image, StatusBar, TextInput, Touchable, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, StatusBar, TextInput, Touchable, TouchableOpacity } from 'react-native';
 import { Linking, StyleProp, TextStyle, ViewStyle, } from 'react-native';
-import { Header as HeaderRNE, HeaderProps, Icon } from 'react-native-elements';
+import { FlatList, ActivityIndicator } from 'react-native';
+import { Header as HeaderRNE, HeaderProps, Icon, Image, SafeAreaView, Card, Button } from 'react-native-elements';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 export default function HomeScreen({ navigation }) {
 
     return (
-        
+      
     <SafeAreaProvider>
       <HeaderRNE backgroundColor='#c41414' 
         leftComponent={
@@ -28,7 +29,40 @@ export default function HomeScreen({ navigation }) {
         }
        //centerComponent={{ text: 'Header', style: styles.heading }}
       />
+      <Image style={styles.imagem}
+        source={{ uri:  'http://www.qualviagem.com.br/wp-content/uploads/2015/11/Olinda7_Marcio-Silva.jpg'}}
+        containerStyle={styles.item}
+        PlaceholderContent={<ActivityIndicator />}
+      /> 
 
+      <TextInput
+            style={styles.input}  
+            placeholder="Search..."
+      />
+    <View style={styles.card}>
+      <Card>
+        <Card.Title>PRAÇA BELA VISTA</Card.Title>
+        <Card.Divider />
+        <Card.Image
+          style={{ padding: 0 }}
+          source={{
+            uri:
+                'https://www.feriasbrasil.com.br/fotosfb/802222889-MOB.jpg',
+            }}
+        />  
+      </Card>
+      <Card>
+        <Card.Title>CATEDRAL CENTRAL</Card.Title>
+        <Card.Divider />
+        <Card.Image
+          style={{ padding: 0 }}
+          source={{
+            uri:
+                'https://media-cdn.tripadvisor.com/media/photo-s/06/0f/8c/de/catedral-igreja-sagrado.jpg',
+            }}
+        />  
+      </Card>
+    </View>  
     </SafeAreaProvider>
     );
 }
@@ -96,5 +130,42 @@ const styles = StyleSheet.create({
         color: 'white',
         fontSize: 16,
         fontWeight: 'bold',
+      },
+      list: {
+        width: '100%',
+        backgroundColor: '#000',
+      },
+      item: {
+        aspectRatio: 1,
+        width: '100%',
+        height: '50%',
+        flex: 1,
+      },
+      imagem: {
+        height: '25%'
+      },
+      input: {
+        backgroundColor: '#fff',
+        width: '60%',
+        height: 30,
+        paddingLeft: 10,
+        borderRadius: '2%',
+        position: 'absolute',
+        left:     83,
+        top:      210,
+        shadowColor: '#000',
+        shadowOffset: {
+          width: 0,
+          height: 8
+        },
+        shadowOpacity: 0.58,
+        shadowRadius: 16.00,
+        elevation: 10
+      },
+      card: {
+        position: 'absolute',
+        left: 6,
+        top:      260,
+        width: 400
       }
 });
