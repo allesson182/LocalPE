7c8e1436f43c7d9c070d09f0c7e8bb2a35a834c6
Roberta Vitória <66263681+roberta976@users.noreply.github.com>
2022-01-10 22:40:22 -0300

    atualizaçao da tela inicial

--- screens/homeScreen.js
+++ screens/homeScreen.js
@@ -1,77 +1,97 @@
 import { NavigationContainer } from '@react-navigation/native';
-import React, {useState} from 'react';
-import { StyleSheet, Text, View, StatusBar, TextInput, Touchable, TouchableOpacity } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, View, StatusBar, TextInput, Touchable, TouchableOpacity } from 'react-native';
 import { Linking, StyleProp, TextStyle, ViewStyle, } from 'react-native';
 import { FlatList, ActivityIndicator } from 'react-native';
-import { Header as HeaderRNE, HeaderProps, Icon, Image, SafeAreaView, Card, Button } from 'react-native-elements';
+import { Header as HeaderRNE, HeaderProps, Icon, SafeAreaView, Card } from 'react-native-elements';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import {
+    Avatar,
+    Button,
+    Image,
+    NativeBaseProvider,
+    Box,
+    Text,
+    Input,
+    Heading,
+    VStack,
+    FormControl,
+    Link,
+    IconButton,
+    HStack,
+    Divider,
+} from 'native-base';
 
 export default function HomeScreen({ navigation }) {
 
-  const list = [
-    {
-      name: 'Olinda',
-      avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
-      subtitle: 'Vice President'
-    },
-    ,{
-      name: 'Recife',
-      avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
-      subtitle: 'Vice Chairman'
-    },
-   
-  ]
+    const list = [
+        {
+            name: 'Olinda',
+            avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
+            subtitle: 'Vice President'
+        },
+        , {
+            name: 'Recife',
+            avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
+            subtitle: 'Vice Chairman'
+        },
+
+    ]
 
     return (
-      
-    <SafeAreaProvider>
-      <HeaderRNE backgroundColor='#c41414' 
-        leftComponent={
-          <View>
-              <Text style={styles.titulo}>Local PE</Text>
-          </View>
-        }
-        rightComponent={
-            <View style={styles.headerRight}>
-              <TouchableOpacity style={styles.botaoLogin}>
-                <Text style={styles.textoBotaoLogin}>Log in</Text>
-              </TouchableOpacity>
-              <TouchableOpacity style={styles.botaoCadastrar}>
-                <Text style={styles.textoBotaoCadastrar}>Cadastrar</Text>
-              </TouchableOpacity>
-            </View>
-        }
-       //centerComponent={{ text: 'Header', style: styles.heading }}
-      />
-      <Image style={styles.imagem}
-        source={{ uri:  'http://www.qualviagem.com.br/wp-content/uploads/2015/11/Olinda7_Marcio-Silva.jpg'}}
-        containerStyle={styles.item}
-        PlaceholderContent={<ActivityIndicator />}
-      /> 
 
-      <TextInput
-            style={styles.input}  
-            placeholder="Search..."
-      />
-    <View style={styles.card}>
-    {
-    list.map((l, i) => (
-     
-      <Card>
-        <Card.Title>{l.name}</Card.Title>
-        <Card.Divider />
-        <Card.Image
-          style={{ padding: 0 }}
-          source={{
-            uri:
-                'https://www.feriasbrasil.com.br/fotosfb/802222889-MOB.jpg',
-            }}
-        />  
-      </Card>
-       ))
-      }
-    </View>  
-    </SafeAreaProvider>
+        <NativeBaseProvider>
+            <HeaderRNE backgroundColor='#EA4335'
+                leftComponent={
+                    <View>
+                        <Text style={styles.titulo}>Local PE</Text>
+                    </View>
+                }
+                rightComponent={
+                    <View style={styles.headerRight}>
+                        <TouchableOpacity style={styles.botaoLogin} onPress={() => navigation.navigate('login')}>
+                            <Text style={styles.textoBotaoLogin}>Log In</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity style={styles.botaoCadastrar} onPress={() => navigation.navigate('cadastro')}>
+                            <Text style={styles.textoBotaoCadastrar}>Cadastrar</Text>
+                        </TouchableOpacity>
+                    </View>
+                }
+            //centerComponent={{ text: 'Header', style: styles.heading }}
+            />
+            < Image style={styles.imagem}
+                source={{ uri: 'http://www.qualviagem.com.br/wp-content/uploads/2015/11/Olinda7_Marcio-Silva.jpg' }}
+                containerStyle={styles.item}
+                PlaceholderContent={< ActivityIndicator />}
+            />
+
+            < TextInput
+                style={styles.input}
+                placeholder="Search..."
+            />
+            <VStack alignItems="center">
+                <View style={styles.card}>
+                    {
+                        list.map((l, i) => (
+
+                            <Card>
+                                <Card.Title>{l.name}</Card.Title>
+                                <Card.Divider />
+                                <Card.Image
+                                    style={{ padding: 0 }}
+                                    source={{
+                                        uri:
+                                            'https://www.feriasbrasil.com.br/fotosfb/802222889-MOB.jpg',
+                                    }}
+                                />
+                            </Card>
+                        ))
+                    }
+                </View>
+            </VStack>
+
+
+        </NativeBaseProvider >
     );
 }
 
@@ -91,7 +111,7 @@ const styles = StyleSheet.create({
     },
     textoBotaoLogin: {
         color: 'black',
-        
+
     },
     botaoLogin: {
         marginRight: 5,
@@ -113,7 +133,7 @@ const styles = StyleSheet.create({
         height: 30,
         alignItems: 'center',
         justifyContent: 'center',
-        backgroundColor: '#c41414',
+        backgroundColor: '#EA4335',
         borderBottomColor: '#fff'
     },
     headerContainer: {
@@ -123,57 +143,58 @@ const styles = StyleSheet.create({
         marginBottom: 20,
         width: '100%',
         paddingVertical: 15,
-      },
-      heading: {
+    },
+    heading: {
         color: 'white',
         fontSize: 22,
         fontWeight: 'bold',
-      },
-      headerRight: {
+    },
+    headerRight: {
         display: 'flex',
         flexDirection: 'row',
         marginTop: 5,
-      },
-      subheaderText: {
+    },
+    subheaderText: {
         color: 'white',
         fontSize: 16,
         fontWeight: 'bold',
-      },
-      list: {
+    },
+    list: {
         width: '100%',
         backgroundColor: '#000',
-      },
-      item: {
+    },
+    item: {
         aspectRatio: 1,
         width: '100%',
         height: '50%',
         flex: 1,
-      },
-      imagem: {
+    },
+    imagem: {
         height: '25%'
-      },
-      input: {
+    },
+    input: {
         backgroundColor: '#fff',
         width: '60%',
         height: 30,
+        alignSelf: 'center',
         paddingLeft: 10,
         borderRadius: '2%',
         position: 'absolute',
-        left:     83,
-        top:      210,
+
+        top: 190,
         shadowColor: '#000',
         shadowOffset: {
-          width: 0,
-          height: 8
+            width: 0,
+            height: 8
         },
         shadowOpacity: 0.58,
         shadowRadius: 16.00,
         elevation: 10
-      },
-      card: {
-        position: 'absolute',
-        left: 6,
-        top:      260,
-        width: 400
-      }
+    },
+    card: {
+        marginTop: '10px',
+        padding: '15px',
+        width: '100%',
+
+    }
 });
